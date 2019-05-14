508213429bcedd43f71093e79214ddc8b7c67da2
Anton Weiss <anton@otomato.link>
2019-05-14 15:22:30 +0300

    Setup for kubeadm

---
+++ prepare-vms/setup_kubeadm.sh
@@ -0,0 +1,57 @@
+#! /bin/bash -f
+
+sudo apt-get update
+
+#Install packages to allow apt to use a repository over HTTPS:
+sudo apt-get install -y apt-transport-https \
+	                ca-certificates \
+		        curl \
+		        software-properties-common \
+                        jq \
+			pkg-config
+
+#Add Docker’s official GPG key:
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+	   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+	      $(lsb_release -cs) \
+	         stable"
+
+sudo apt-get update && sudo apt-get install -y docker-ce=18.06.3~ce~3-0~ubuntu
+
+#install compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo add-apt-repository \
+           "deb [arch=amd64] https://apt.kubernetes.io/ \
+		      kubernetes-xenial \
+			    main"
+
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl kubernetes-cni
+
+echo "source <(kubectl completion bash)" >> ~/.bashrc
+
+sudo adduser $USER docker
+#install kube-ps1
+cd ~/
+git clone https://github.com/jonmosco/kube-ps1.git
+echo 'source ~/kube-ps1/kube-ps1.sh' >> ~/.bashrc
+echo "PS1='[\u@\h \W \$(kube_ps1)]\$ '" >> ~/.bashrc
+cd -
+
+#install kubens and kubectx
+git clone https://github.com/ahmetb/kubectx.git ~/.kubectx
+COMPDIR=$(pkg-config --variable=completionsdir bash-completion)
+sudo ln -sf ~/.kubectx/completion/kubens.bash $COMPDIR/kubens
+sudo ln -sf ~/.kubectx/completion/kubectx.bash $COMPDIR/kubectx
+cat << FOE >> ~/.bashrc
+
+
+#kubectx and kubens
+export PATH=~/.kubectx:\$PATH
+FOE
