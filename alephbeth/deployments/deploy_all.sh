98bb95090e35141641a45fbbd7baf280b07eebfe
Anton Weiss <anton@otomato.link>
2019-05-20 13:11:22 +0300

    App deployment flow

--- alephbeth/deployments/deploy_all.sh
+++ alephbeth/deployments/deploy_all.sh
@@ -1,4 +1,4 @@
 #! /bin/bash -f
-kubectl create ns games
-kubectl label ns games istio-injection=enabled
-kubectl apply -f aleph-dep.yml  -f front-dep.yml -f beth-dep.yml -f loader-dep.yml -n games
+kubectl create ns staging
+kubectl label ns staging istio-injection=enabled
+kubectl apply -f aleph.yaml  -f front.yaml -f beth.yaml  -n staging 
