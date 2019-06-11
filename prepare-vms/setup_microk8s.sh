223d07a9e20e5e3d4164f87fc7b6377bba6fac5f
Anton Weiss <anton@otomato.link>
2019-06-11 07:27:31 +0200

    Fix docker permissions

--- prepare-vms/setup_microk8s.sh
+++ prepare-vms/setup_microk8s.sh
@@ -1,10 +1,11 @@
 #! /bin/bash -f
 #install docker (for building images)
 sudo snap install docker
-sudo setfacl -m user:${USER}:rw /var/run/docker.sock
 sudo snap install microk8s --classic                                                                                                         
 sudo snap install kubectl --classic
 echo "source <(kubectl completion bash)" >> ~/.bashrc                                                                                                          
 microk8s.start                                                                                                                               
 microk8s.enable istio                                                                                                          
 microk8s.enable metrics-server
+# allow current user to access docker socket
+sudo setfacl -m user:${USER}:rw /var/run/docker.sock
