018da95082446b88394a1fa91208cf88d2f70a9e
Ant Weiss <ant.weiss@gmail.com>
2019-09-26 22:14:08 +0300

    Update setup_microk8s.sh

--- prepare-vms/setup_microk8s.sh
+++ prepare-vms/setup_microk8s.sh
@@ -3,10 +3,12 @@
 sudo snap install docker
 sudo snap install microk8s --classic                                                                                                         
 sudo snap install kubectl --classic
-echo "source <(kubectl completion bash)" >> ~/.bashrc                                                                                                          
-microk8s.start                                                                                                                               
-microk8s.enable istio                                                                                                          
-microk8s.enable metrics-server
+echo "source <(kubectl completion bash)" >> ~/.bashrc
+sudo microk8s.start
+sudo microk8s.enable istio
+sudo microk8s.kubectl config view --raw > $HOME/.kube/config
+sleep 5
+sudo microk8s.enable metrics-server
 # allow current user to access docker socket
 sudo setfacl -m user:${USER}:rw /var/run/docker.sock
 # allow istio pods to communicate
