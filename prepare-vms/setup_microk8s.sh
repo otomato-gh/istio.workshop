d38493332db23251cd530db09a7378c00e1975ed
Ant Weiss <ant.weiss@gmail.com>
2019-10-03 20:06:48 +0300

    Add robustness to microk8s setup script

--- prepare-vms/setup_microk8s.sh
+++ prepare-vms/setup_microk8s.sh
@@ -1,22 +1,24 @@
 #! /bin/bash -f
 #install docker (for building images)
 sudo snap install docker
+# allow current user to access docker socket
+sudo setfacl -m user:${USER}:rw /var/run/docker.sock
 sudo snap install microk8s --classic                                                                                                         
 sudo snap install kubectl --classic
 echo "source <(kubectl completion bash)" >> ~/.bashrc
 sudo microk8s.start
-sleep 2
-sudo microk8s.enable dns
-sleep 2
+# allow microk8s pods to communicate
+sudo iptables -P FORWARD ACCEPT
+# wait before enabling DNS
+sleep 5
+# retry enabling DNS (sometimes fails on first try
+sudo microk8s.enable dns || sudo microk8s.enable dns
+sleep 5
 sudo microk8s.enable istio
 sudo chown -R $USER $HOME/.kube
 sudo microk8s.kubectl config view --raw > $HOME/.kube/config
 sudo chown $USER $HOME/.kube/config
 #wait before enabling metrics server
-sleep 2
+sleep 5
 sudo microk8s.enable metrics-server
 sudo usermod -a -G microk8s $USER
-# allow current user to access docker socket
-sudo setfacl -m user:${USER}:rw /var/run/docker.sock
-# allow istio pods to communicate
-sudo iptables -P FORWARD ACCEPT
