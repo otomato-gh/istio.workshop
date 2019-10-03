bd4ed5919183f1841dbdf9bc7c5478ee4de56f9a
Ant Weiss <ant.weiss@gmail.com>
2019-10-03 20:10:38 +0300

    Try again to make microk8s setup more robust

--- prepare-vms/setup_microk8s.sh
+++ prepare-vms/setup_microk8s.sh
@@ -1,8 +1,6 @@
 #! /bin/bash -f
 #install docker (for building images)
 sudo snap install docker
-# allow current user to access docker socket
-sudo setfacl -m user:${USER}:rw /var/run/docker.sock
 sudo snap install microk8s --classic                                                                                                         
 sudo snap install kubectl --classic
 echo "source <(kubectl completion bash)" >> ~/.bashrc
@@ -14,11 +12,15 @@ sleep 5
 # retry enabling DNS (sometimes fails on first try
 sudo microk8s.enable dns || sudo microk8s.enable dns
 sleep 5
-sudo microk8s.enable istio
+sudo microk8s.enable istio || sudo microk8s.enable istio
 sudo chown -R $USER $HOME/.kube
 sudo microk8s.kubectl config view --raw > $HOME/.kube/config
 sudo chown $USER $HOME/.kube/config
 #wait before enabling metrics server
 sleep 5
-sudo microk8s.enable metrics-server
+sudo microk8s.enable metrics-server || sudo microk8s.enable metrics-server
 sudo usermod -a -G microk8s $USER
+# allow current user to access docker socket
+sudo setfacl -m user:${USER}:rw /var/run/docker.sock
+# allow microk8s pods to communicate
+sudo iptables -P FORWARD ACCEPT
