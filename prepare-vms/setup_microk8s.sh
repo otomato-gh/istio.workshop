0ec35f90fc040bc0e2894f5eaba193198191725a
Anton Weiss <anton@otomato.link>
2019-10-07 08:41:09 +0100

    Remove redundant sleeps in microk8s setup

--- prepare-vms/setup_microk8s.sh
+++ prepare-vms/setup_microk8s.sh
@@ -9,16 +9,13 @@ sudo microk8s.start
 sudo iptables -P FORWARD ACCEPT
 # wait before enabling DNS
 sleep 5
-# retry enabling DNS (sometimes fails on first try
-sudo microk8s.enable dns || sudo microk8s.enable dns
-sleep 5
-sudo microk8s.enable istio || sudo microk8s.enable istio
+sudo microk8s.enable dns
+sudo microk8s.enable istio
 sudo chown -R $USER $HOME/.kube
 sudo microk8s.kubectl config view --raw > $HOME/.kube/config
 sudo chown $USER $HOME/.kube/config
 #wait before enabling metrics server
-sleep 5
-sudo microk8s.enable metrics-server || sudo microk8s.enable metrics-server
+sudo microk8s.enable metrics-server
 sudo usermod -a -G microk8s $USER
 # allow current user to access docker socket
 sudo setfacl -m user:${USER}:rw /var/run/docker.sock
